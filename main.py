793ccb25645a5953ee993b00e4852b437235a9e0
Daniel Gonzalez <daniel.gonzalez@appdynamics.com>
2024-01-28 14:29:38 -0800

    add req.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import pandas_ta as ta
 import numpy as np
 from datetime import date
-import pandas_ta as ta
 from jugaad_data.nse import stock_df
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import MinMaxScaler
@@ -11,7 +11,7 @@ from config import NEIGHBOURS_COUNT, MAX_BARS_BACK
 def preprocess_data(df, max_bars_back):
     # Reverse and truncate the DataFrame
     df = df.iloc[::-1]
-    df = df.tail(max_bars_back + 50).reset_index(drop=True)
+    df = df.tail(max_bars_back).reset_index(drop=True)
 
     # Scale the data
     scaler = MinMaxScaler()
@@ -23,7 +23,7 @@ def calculate_technical_indicators(df, rsi_period=14, cci_period=20, rsi_period2
     ndf = pd.DataFrame()
     ndf['RSI'] = ta.rsi(df['CLOSE'], length=rsi_period)
     ndf['CCI'] = ta.cci(df['HIGH'], df['LOW'], df['CLOSE'], length=cci_period)
-    ndf['RSI2'] = ta.rsi(df['CLOSE'], length=rsi_period2)    
+    ndf['RSI2'] = ta.rsi(df['CLOSE'], length=rsi_period2)
     return ndf
 
 def train_lorentzian_model(ndf, n_neighbors):
@@ -56,7 +56,7 @@ def make_predictions(model, ndf, future_candle=10):
     
     return res, time
 if __name__ == "__main__":
-    price_df = stock_df(symbol="SBIN", from_date=date(2020, 1, 1), to_date=date(2023, 10, 1), series="EQ")
+    price_df = stock_df(symbol="SBIN", from_date=date(2022, 1, 1), to_date=date(2023, 10, 1), series="EQ")
     df = preprocess_data(price_df, max_bars_back=MAX_BARS_BACK)
     ndf = calculate_technical_indicators(df)
     model = train_lorentzian_model(ndf, n_neighbors=NEIGHBOURS_COUNT)
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     predictions, time = make_predictions(model, ndf)
     
     plt.plot(time[0], predictions[0], label='Expected', color='blue')
-    plt.plot(time[0], price_df['CLOSE'].iloc[-len(predictions[0]):], label='Actual', color='red')
+    plt.plot(time[0], price_df['CLOSE'].iloc[-len(predictions[0]):], label='Predicted', color='red')
     plt.xlabel('Time')
     plt.ylabel('Price')
     plt.legend()
